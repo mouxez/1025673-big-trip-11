6ebe6a5e5f343f301e697c030debc8bc0e323bc1
Maksim <mouxez@gmail.com>
2020-05-01 19:47:14 +0300

    integrate controller structure

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -1,20 +1,38 @@
-import TripPointComponent from '../components/trip-point.js';
-import TripEditComponent from '../components/trip-edit.js';
-import {render, RenderPosition, replace} from '../utils/render.js';
+import {render, RenderPosition, replace, remove} from '../utils/render.js';
 import {getRandomInteger} from '../utils/common.js';
 import {MAX_ROUTE_COUNT} from '../const.js';
 import {sortedByStartTime} from '../main.js';
+import TripPointComponent from '../components/trip-point.js';
+import TripEditComponent from '../components/trip-edit.js';
+import MenuComponent from '../components/menu.js';
+import FilterComponent from '../components/filter.js';
+import SortingComponent from '../components/sorting.js';
+import TripOfferComponent from '../components/trip-offer.js';
+import NoTripPointsComponent from '../components/no-trip-points.js';
+
+const tripEvent = document.querySelector(`.trip-events`);
 
 export default class Controller {
   constructor(container) {
     this._container = container;
+    this._menuComponent = new MenuComponent();
+    this._filterComponent = new FilterComponent();
+    this._sortingComponent = new SortingComponent();
+    this._tripOfferComponent = new TripOfferComponent();
+    this._noTripPointsComponent = new NoTripPointsComponent();
   }
   render(container) {
+    render(container, this._menuComponent, RenderPosition.BEFOREEND);
+    render(container, this._filterComponent, RenderPosition.BEFOREEND);
+    render(tripEvent, this._sortingComponent, RenderPosition.BEFOREEND);
+    render(tripEvent, this._tripOfferComponent, RenderPosition.BEFOREEND);
+
     sortedByStartTime.slice(0, getRandomInteger(0, MAX_ROUTE_COUNT)).forEach((i) => {
       let tripPointComponent = new TripPointComponent(i);
       let tripEditComponent = new TripEditComponent(i);
+      const tripEventPoint = document.querySelector(`.trip-events__list`);
 
-      render(container, tripPointComponent, RenderPosition.BEFOREEND);
+      render(tripEventPoint, tripPointComponent, RenderPosition.BEFOREEND);
 
       const onEditFormSubmit = (evt) => {
         evt.preventDefault();
@@ -35,5 +53,13 @@ export default class Controller {
       };
       tripPointComponent.setEditButtonClickHandler(onEditButtonClick);
     });
+  }
+  renderCreateFirstPointMessage(nodeArray) {
+    const tripEvents = document.querySelector(`.trip-events`);
+    if (nodeArray.length === 0) {
+      remove(this._tripOfferComponent);
+      remove(this._sortingComponent);
+      render(tripEvents, this._noTripPointsComponent, RenderPosition.BEFOREEND);
+    }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,41 +1,15 @@
 import ControllerComponent from './controllers/trip.js';
-import MenuComponent from './components/menu.js';
-import FilterComponent from './components/filter.js';
-import SortingComponent from './components/sorting.js';
-import TripOfferComponent from './components/trip-offer.js';
-import NoTripPointsComponent from './components/no-trip-points.js';
 import {DATA_COUNT} from '../src/const.js';
 import {createData} from './mock/trip-point.js';
 import {sortByStartTime} from './utils/common.js';
-import {render, RenderPosition, remove} from './utils/render.js';
 
-const menuComponent = new MenuComponent();
-const filterComponent = new FilterComponent();
-const sortingComponent = new SortingComponent();
-const tripOfferComponent = new TripOfferComponent();
-const noTripPointsComponent = new NoTripPointsComponent();
 const controllerComponent = new ControllerComponent();
-const tripControls = document.querySelector(`.trip-controls`);
-const tripEvent = document.querySelector(`.trip-events`);
 
 let listOfData = createData(DATA_COUNT);
 export let sortedByStartTime = listOfData.sort(sortByStartTime);
 
-render(tripControls, menuComponent, RenderPosition.BEFOREEND);
-render(tripControls, filterComponent, RenderPosition.BEFOREEND);
-render(tripEvent, sortingComponent, RenderPosition.BEFOREEND);
-render(tripEvent, tripOfferComponent, RenderPosition.BEFOREEND);
-
-const tripEventPoint = document.querySelector(`.trip-events__list`);
-controllerComponent.render(tripEventPoint);
+const tripControls = document.querySelector(`.trip-controls`);
+controllerComponent.render(tripControls);
 
 const listOfPoints = Array.from(document.querySelectorAll(`.trip-events__item`));
-const renderCreateFirstPointMessage = (nodeArray) => {
-  const tripEvents = document.querySelector(`.trip-events`);
-  if (nodeArray.length === 0) {
-    remove(tripOfferComponent);
-    remove(sortingComponent);
-    render(tripEvents, noTripPointsComponent, RenderPosition.BEFOREEND);
-  }
-};
-renderCreateFirstPointMessage(listOfPoints);
+controllerComponent.renderCreateFirstPointMessage(listOfPoints);
