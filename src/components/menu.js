5c2ecb2da4021b3cc68951ca83cb2b82018605b3
Maksim Borovik <mouxez@gmail.com>
2020-05-22 07:19:18 +0300

    remove constructor

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,14 +1,10 @@
 import AbstractComponent from "./abstract-component.js";
 
 export default class Menu extends AbstractComponent {
-  constructor(values) {
-    super();
-    this._values = values;
-  }
-
   getTemplate() {
     return `<nav class="trip-controls__trip-tabs  trip-tabs">
-    ${this._values.map((value) => `<a class="trip-tabs__btn  ${value.active ? `trip-tabs__btn--active` : ``}" href="#">${value.title}</a>`).join(``)}
+    <a class="trip-tabs__btn trip-tabs__btn--active" href="#">Table</a>
+    <a class="trip-tabs__btn" href="#">Stats</a>
 
     </nav>`;
   }
